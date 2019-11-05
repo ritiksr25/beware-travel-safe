6e4b0efb47a361668fe3efc6d0ef8aa7b7c0b552
shubham100ev <shubhamsingh19981110@gmail.com>
2019-11-05 18:16:24 +0530

    sync master

---
+++ server/routes/api/v1/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+// load controller
+const { index } = require("../../../controllers/index_controller");
+// middlewares
+
+// routes
+
+// export router
+module.exports = router;
