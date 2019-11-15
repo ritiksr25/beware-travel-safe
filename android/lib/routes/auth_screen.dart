e143621ca5d50026cfa6362fbbd2c5be4eb3c96b
ritik srivastava <ritiksrivastava025@gmail.com>
2019-11-15 21:53:11 +0530

    merge conflicts

--- android/lib/routes/auth_screen.dart
+++ android/lib/routes/auth_screen.dart
@@ -32,70 +32,6 @@ class _AuthScreenState extends State<AuthScreen> {
                       borderSide:
                           BorderSide(color: const Color(0xFF7B65E4), width: 1),
                     ),
-<<<<<<< HEAD
-                    filled: true,
-                    fillColor: Colors.white,
-                    border: const OutlineInputBorder(
-                      borderSide: const BorderSide(
-                          color: const Color(0xFF707070), width: 0.2),
-                    ),
-                    hintText: "Email",
-                    prefixIcon: const Icon(
-                      Icons.email,
-                      color: const Color(0xFF7B65E4),
-                    ),
-                  ),
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.only(top: 20.0, left: 24.0, right: 24.0),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.white,
-                    focusedBorder: OutlineInputBorder(
-                      borderSide:
-                          BorderSide(color: const Color(0xFF7B65E4), width: 1),
-                    ),
-                    border: const OutlineInputBorder(
-                      borderSide: const BorderSide(
-                          color: const Color(0xFF707070), width: 0.2),
-                    ),
-                    hintText: "Password",
-                    prefixIcon: const Icon(
-                      Icons.vpn_key,
-                      color: const Color(0xFF7B65E4),
-                    ),
-                  ),
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.only(top: 36.0, left: 24.0, right: 24.0),
-                child: SizedBox(
-                  
-                  width: double.infinity,
-                  child: RaisedButton(
-                    padding: EdgeInsets.symmetric(vertical: 8, horizontal: 48),
-                    child: Text(
-                      "Log in",
-                      style: TextStyle(
-                        fontSize: 24.0,
-                        fontFamily: 'Product Sans',
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
-                    color: const Color(0xFF7B65E4),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: new BorderRadius.circular(8.0)),
-                    onPressed: () {},
-                  ),
-                ),
-              )
-            ],
-          ),
-        ],
-=======
                   ),
                   Container(
                     padding:
@@ -211,7 +147,6 @@ class _AuthScreenState extends State<AuthScreen> {
             ),
           ],
         ),
->>>>>>> android
       ),
     );
   }
